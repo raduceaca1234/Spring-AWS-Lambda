1815b213e622cd0e91f7bd8cca70209663c592ed
raduceaca1234 <ceaca.radu@yahoo.com>
2024-03-08 17:16:08 +0200

    Initialize the project

---
+++ java-spring-on-lambda/src/main/java/org/raduceaca/Application.java
@@ -0,0 +1,19 @@
+package org.raduceaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import org.raduceaca.controller.PingController;
+
+
+@SpringBootApplication
+// We use direct @Import instead of @ComponentScan to speed up cold starts
+// @ComponentScan(basePackages = "org.raduceaca.controller")
+@Import({ PingController.class })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
